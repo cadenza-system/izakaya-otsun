63bf89c1313f203430459265a7bb9d339dc2a054
dai-1103 <bosatsunumnum@gmail.com>
2023-10-05 02:54:46 +0900

    いろいろ調整

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".mypattern").slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    speed: 800,
+    dots: true,
+    arrows: true,
+    centerMode: true,
+    centerPadding: "20%",
+  });
+});
